8649b3481b86108d51d2fdff0b8504092ebb169e
agupta9889 <arungupta.info000@gmail.com>
2021-12-15 19:03:29 +0530

    check update profile doctor

--- src/pages/DoctorProfile.jsx
+++ src/pages/DoctorProfile.jsx
@@ -1,116 +1,59 @@
 import React, { useState, useEffect } from "react";
+import FormData from "form-data";
 import { useParams } from "react-router";
 import axios from "axios";
-import companyLogoIcon from '../assets/images/Carebee-blue-icon.png';
-import { Container, Row, Col, Media, Card, CardBody, Badge, Button } from "reactstrap";
+import swal from "sweetalert";
+import companyLogoIcon from "../assets/images/Carebee-blue-icon.png";
+import {
+  Container,
+  Row,
+  Col,
+  Media,
+  Card,
+  CardBody,
+  Badge,
+  Input,
+  Button,
+  Form,
+  FormGroup,
+  Label,
+} from "reactstrap";
 import Sidebar from "../components/Sidebar";
-import GLOBALS from '../constants/global';
+import GLOBALS from "../constants/global";
 
 const DoctorProfile = () => {
-	
-	const {id} = useParams();
-	const [lan, setLan] = useState();
+	const [uploadImage, setUploadImages] = React.useState();
 
-	useEffect(() => {
-		loadDoctorProfile();
-	}, []);
+	const BaseURL = "http://192.168.137.1:5000/";
+	const { id } = useParams();
 
-	const[profile, setProfile] = useState({});
-	
-	const loadDoctorProfile = async () => {
-			
-		var config = {
-		  method: 'get',
-		  url: `${GLOBALS.BASE_URL}/user/getbyid/` + id,
-		  headers: { 
-			'x-auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNzY4YjlhYjgyYmQwMDJkMGU0ZmFhYiIsImlhdCI6MTYzNTE2NTk2NSwiZXhwIjo2ODE5MTY1OTY1fQ._Jy0lEA0y8ojQqauoDUKyEuujKxcZfzT55ISt2hMuZo', 
-			'Content-Type': 'application/json'
-		  },
-		 
-		};
-		
-		axios(config)
-		.then(response => {
-		  console.log('Response in doctor profile :::', response.data.data);
-		  if (response.data.data) {
-			setProfile(response.data.data);
-			const ln = response.data.data.language;
-			const split = ln.join(", ")
-			setLan(split);
-			//console.log("language is ::::::::::: ",split);
-		  }
-		})
-		.catch(function (error) {
-		  console.log(error);
-		});
+	const onChangeFile = (event) => {
+	  if (alert('Are you want to update image')) {		
+		  console.log('onchagne', event.target.files[0])
+			setUploadImages(event.target.files[0]);
+		}
+	};
 
-	}	
-	
-return (
-	
-	<>
-		<Sidebar />
-		<Container >
-			<Row >
-				<Col md={2} xs={1}></Col>
-				<Col md={10} xs={10} className="doctor-top">
-					<Row>
-						<Col md={7} xs={12} >
-							<Media className="mb-2">
-								<a href="/" className="mr-1">
-									<img src={'http://192.168.137.1:5000/'+ profile.profileImage} alt="users view avatar" className="users-avatar-shadow rounded-circle" height="64" width="64"/>
-								</a>
-								<Media body className="pt-25">
-									<Media heading>Dr. {profile.firstName} {profile.lastName}</Media>
-									<span>Qualification : {profile.qualification}</span>
-								</Media>
-							</Media>
-						</Col>
-						{/* <Col md={5} xs={12} className="d-flex justify-content-end align-items-center mb-2">
-							<Button outline color="info" size="sm" style={{ float: "right" }}>Edit</Button>
-						</Col> */}
-					</Row>   
-					<Card className="shadow doctor-top">
-						<CardBody>
-							<Row className="bg-lighten-5 rounded mb-2 mx-25">
-								<Col md={4} sm={12} className="p-2">
-									<span><b>Mobile:</b> {profile.mobile}</span>
-								</Col>
-								<Col md={4} sm={12} className="p-2"><b>Email:</b> {profile.email}</Col>
-								<Col md={4} sm={12} className="p-2"><b>Experience:</b> {profile.experience} Years</Col>
-							</Row>
-							<Row className="doctor-top mb-2 mx-25">
-								<Col md={2}>Gender:</Col>
-								<Col md={10}>{profile.gender}</Col>
-								<Col md={2}>Qualification:</Col>
-								<Col md={10}>{profile.qualification}</Col>
-								<Col md={2}>Specialities:</Col>
-								<Col md={10}>{profile.specialties}</Col>
-								<Col md={2}>Language:</Col>
-								
-								<Col md={10}>
-									{lan ? lan : "English" }	
+ 
 
-								</Col>
-								<Col md={2}>Bio:</Col>
-								<Col md={10}>{profile.about}</Col>
-								<Col md={2}>Status:</Col>
-								<Col md={10}>
-									{
-										(profile.status === 0)  // 0-Active, 1-Inactive
-										?<Badge style={{backgroundColor: "green"}}>Active</Badge>
-										:<Badge style={{backgroundColor: "red"}}>Inactive</Badge>
-									}	
-								</Col>
-							</Row>
-						</CardBody>
-					</Card>
-				</Col>
-				<Col xs={1}></Col>
-			</Row>
-		</Container>
-	</>
-);
+  return (
+    <>
+      <Container>
+        <Row>
+          <Form>
+            <FormGroup className="col-md-3">
+              <input type="file" name="file" onChange={onChangeFile} />
+            </FormGroup>{" "}
+            <FormGroup className="col-md-3">
+              {/* <Button type="submit" className="btn-success">
+                Upload Image
+              </Button> */}
+            </FormGroup>
+          </Form>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default DoctorProfile;
