a556edb4fc4b5a947015c17144115872604375a3
nitinSoftkiwi <nitin@softkiwi.co.in>
2022-07-25 15:18:05 +0530

    Implementation of Appointment module along with pagination and search

--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -1,45 +1,59 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col, Table, Badge } from "reactstrap";
+import { Container, Row, Col, Table, Input } from "reactstrap";
 import Sidebar from "../components/Sidebar";
 import userService from "../services/user";
+import ReactPaginate from "react-paginate";
+import filterServices from "../services/filter";
 
 function Appointments() {
-	
+
 	const [data, setdata] = useState();
+	const [pageCount, setpageCount] =  useState(1);
+	const limit = 10;
+	const type = 'USER';
+	
 	useEffect(() => {
-		getAppointmentData();
+		getAppointmentData(1, limit, type);
 	}, []);
-	const getAppointmentData = async () => {
-		userService.getAppointmentDetails()
+	
+
+	const getAppointmentData = async (currentPage, limit, type) => {
+		if(currentPage == undefined){
+			currentPage = 1 ;
+		}
+		userService.getAppointmentDetails(currentPage, limit, type)
 		.then((response) => {
 			const filterUser = response.data.data;
+			const total = response.data.total;
+			setpageCount(Math.ceil(total/limit));
 			setdata(filterUser);
 		})
 		.catch(function (error) {
 			console.log(error);
 		});	 
 	}
-
-	// const data1 = [{
-	// 	name : "Patient-1",
-	// 	service : "Fever",
-	// 	doctor : "Doctor-1",
-	// 	date : "27-Nov-21",
-	// 	time : "10:30 am",
-	// 	status : "Booked",
-
-	// },
-	// {
-	// 	name : "Patient-2",
-	// 	service : "Pain",
-	// 	doctor : "Doctor-2",
-	// 	date : "27-Nov-21",
-	// 	time : "10:50 am",
-	// 	status : "Canceled",
-
-	// },]
-	// console.log(data1);
-
+	const handlePageClick = async (data) =>{
+		const currentPage = data.selected + 1
+		const usersFromServer = await getAppointmentData(currentPage, limit, type);
+		setdata(usersFromServer);
+	}
+	// Search API
+	const search = async (data) => {
+		if (data.length > 0) {
+			filterServices.searchAppointment(data)
+			.then(response => {
+				const searchUser = response.data.data;
+				const total = response.data.total;
+				setpageCount(Math.ceil(total/limit));
+				setdata(searchUser);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+		} else {
+			getAppointmentData(1, limit, type);
+		}
+	}
 	return (
 		<>
 			<Sidebar />
@@ -47,7 +61,16 @@ function Appointments() {
 			<Row>
 					<Col md={2} xs={1}></Col>
 					<Col md={10} xs={10} className="table-container">
-						<h6>Appointments</h6><hr />
+					<Row>
+						<Col md={7} xs={12} >
+							<h6>Appointments</h6>
+						</Col>
+						<Col md={5} xs={12} className="d-flex justify-content-end align-items-center mb-2">
+							<Input name="search" 
+							onChange={e=> search(e.target.value)} 
+							className="form-control form-control-sm ml-3 w-75 shadow" type="text" placeholder="Search" aria-label="Search" />
+						</Col>
+					</Row>
 						<Table id="patientTable" responsive>
 							<thead>
 								<tr>
@@ -80,6 +103,25 @@ function Appointments() {
 								}
 							</tbody>
 						</Table>
+						<ReactPaginate
+							previousLabel={'<< Pre'}
+							nextLabel={'Next >>'}
+							breakLabel={'...'}
+							pageCount={pageCount}
+							marginPagesDisplayed={2}
+							pageRangeDisplayed={3}
+							onPageChange={handlePageClick}
+							containerClassName={'pagination justify-content-center'}
+							pageClassName={'page-item shadow'}
+							pageLinkClassName={'page-link'}
+							previousClassName={'page-item shadow'}
+							previousLinkClassName={'page-link '}
+							nextClassName={'page-item shadow'}
+							nextLinkClassName={'page-link'}
+							breakClassName={'page-item shadow'}
+							breakLinkClassName={'page-link'}
+							activeClassName={'active shadow'}
+						/>
 					</Col>
 					<Col xs={1}></Col>
 				</Row>
--- src/services/filter.js
+++ src/services/filter.js
@@ -4,4 +4,7 @@ export default {
     searchData(data){
         return Service.get('/user/search/' +data);
     },
+    searchAppointment(data){
+        return Service.get('/user/searchAppointment/' +data);
+    },
 };
